c851383b5392eeb1db797b2b556ffc38d4cc0371
Sierra Laurent <sierraclauren@gmail.com>
2025-03-22 15:08:27 -0700

    fix photo link error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -113,7 +113,7 @@ export default function Home() {
         <div className='flex flex-row w-full justify-between tablet:flex-col tablet:gap-10 tablet:items-center'>
           <Image
               className=' '
-              src='/Icons/logo07.png'
+              src='/Icons/Logo07.png'
               width={300}
               height={300}
               alt=''
